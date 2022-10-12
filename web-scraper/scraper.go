0329ba9091ba4629f13ecd136bf8c423606bc805
Stephanie <yesstephanie1@gmail.com>
2022-10-11 22:37:08 -0700

    updated

--- web-scraper/scraper.go
+++ web-scraper/scraper.go
@@ -16,10 +16,26 @@ func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("j2store.net"),
 	)
-	c.OnHTML("div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
-		fmt.Println(h.Text)
 
+	var items []item
+
+	c.OnHTML("div.col-sm-9 div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
+		item := item{
+			Name:   h.ChildText("h2.product-title"),
+			Price:  h.ChildText("div.sale-price"),
+			ImgURL: h.ChildAttr("img", "src"),
+		}
+		items = append(items, item)
 	})
 
+	c.OnHTML("[title=Next]", func(h *colly.HTMLElement) {
+		next_page := h.Request.AbsoluteURL(h.Attr("href"))
+		c.Visit(next_page)
+	})
+
+	// c.OnRequest()
+
 	c.Visit("http://j2store.net/demo/index.php/shop")
+	fmt.Println(items)
+
 }
